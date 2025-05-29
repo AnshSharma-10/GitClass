832db0a75f5c40e66d95beaaa056579d8bc0f161
Vedant Narayan <vedant.narayan@somaiya.edu>
2025-05-29 14:50:16 +0530

    I am Commited to branch Vedant

--- vedant.py
+++ vedant.py
@@ -1,1 +1,7 @@
 # "HELLO MY NAME IS VEDANT"
+
+a = 10
+def greet():
+    print("HELLO MY NAME IS VEDANT")
+greet()
+# This code defines a variable and a function, then calls the function to print a greeting.
